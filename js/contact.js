a30c79d40fd33aec350bcb9ab277434fe195ffab
Harsha Royal <iharsharoyal@gmail.com>
2026-02-20 17:02:39 +0530

    Initial commit of portfolio code

---
+++ js/contact.js
@@ -0,0 +1,14 @@
+function sendMail(event){
+    event.preventDefault(); // stop default submission
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const subject = `Project inquiry from ${name}`;
+    const body = `Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage: ${message}`;
+
+    const mailtoLink = `mailto:iharsharoyal@gmail.com?subject=${subject}&body=${body}`;
+
+    window.location.href = mailtoLink;
+}
